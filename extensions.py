2e0c79087cf22139aed6c9453000faa97c8a8def
Aliaksei Yafremau <447654972g@gmail.com>
2023-11-12 15:31:05 +0100

    fixing

--- extensions.py
+++ extensions.py
@@ -30,27 +30,12 @@ class Converter:
 
         return rate * amount
 
-    @staticmethod
-    def get_list_of_currencies():
-        url = f"http://apilayer.net/api/list?access_key={getenv('API_ACCESS_KEY')[1:-1]}"
-
-        r = requests.get(url)
-        response = loads(r.content)
-        values = {}
-        reversed_dict = response['currencies']
-        translator = Translator(service_urls=['translate.google.com'])
-        for key in reversed_dict.keys():
-            translation = translator.translate(reversed_dict[key], dest='ru', src='en').text
-            values.update({translation: key})
-        print(values)
-
 def is_float(s: str):
     if s.replace('.', '').isnumeric():
         return True
     else:
         return False
 
-
 if __name__ == '__main__':
     c = Converter()
-    c.get_list_of_currencies()
+    c.get_price('белорусский рубль', 'евро', 4.0)
--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class BotConverter(telebot.TeleBot):
 <имя валюты, цену которой вы хотите узнать> \
 <имя валюты, в которой надо узнать цену первой валюты> \
 <количество первой валюты> \n\
-Для того, чтоб узнать поддерживаемые валюты введите /values'
+Для того, чтоб узнать поддерживаемые валюты введите /values \n\
+Для справки введите /start или /help'
             bot.send_message(message.chat.id, reference)
 
         @self.message_handler(commands=['values'])
@@ -32,10 +33,13 @@ class BotConverter(telebot.TeleBot):
         @self.message_handler(content_types=['text'])
         def convert(message: telebot.types.Message):
             try:
+                text = message.text.lower()
                 possibles_values = {}
                 for currency in currencies:
-                    index_currency = message.text.find(currency)
+                    index_currency = text.find(currency)
                     if index_currency != -1:
+                        if len(set(text.split())) != len(text.split()):
+                            raise ConvertException('Нельзя перевести деньги в ту же валюту')
                         possibles_values.update({index_currency: currency})
 
                 if len(possibles_values) != 2:
@@ -44,10 +48,7 @@ class BotConverter(telebot.TeleBot):
                 base = possibles_values.pop(min(possibles_values.keys()))
                 quote = possibles_values.pop(min(possibles_values.keys()))
 
-                if base == quote:
-                    raise ConvertException('Нельзя перевести деньги в туже валюту')
-
-                amount = message.text.removeprefix(base).strip()
+                amount = text.removeprefix(base).strip()
                 amount = amount.removeprefix(quote).strip()
 
                 if not is_float(amount):
@@ -63,7 +64,8 @@ class BotConverter(telebot.TeleBot):
             except Exception as e:
                 bot.reply_to(message, f'Ошибка на серверной части. {e}')
             else:
-                bot.send_message(message.chat.id, f'{amount} {currencies[base]} будет равно {result} {quote}')
+                bot.send_message(message.chat.id, f'{amount} {currencies[base]}({base})'
+                                                  f' равняется {result} {currencies[quote]}({quote})')
 
 
 bot = BotConverter(getenv('TOKEN'))
